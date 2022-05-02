bb1f5ea2ae03d7fdbfa4a8adf3d33f296359a0a1
Akash Kumar Mohanty <104777123+KingAkash206@users.noreply.github.com>
2022-05-02 20:22:39 +0530

    Add files via upload

---
+++ studentportal.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="flickity/flickity.css">
+	<script type="text/javascript" src="flickity/flickity.js"></script>
+	<title>Library Management</title>
+
+</head>
+<body>
+<div class="container">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example">
+					<span class="sr-only">:</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">SheddyBen Lib-BookSys</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="profile.php">View Profile</a></li>
+					<li><a href="borrow-student.php">Borrow Books</a></li>					
+					<li><a href="fine-student.php">Fines</a></li>					
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</div>
+
+		<div class="container-fluid slide">
+			
+	  		<div class="slider">
+	  			<!-- <h1>Flickity - wrapAround</h1> -->
+
+
+					<div class="carousel" data-flickity='{ "autoPlay": true }'; >
+
+						  <div class="carousel-cell" auto-play >
+						  	<img src="images/header-bg.jpg">
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	<img src="images/header-bg.jpg">
+
+						  </div>
+						  <div class="carousel-cell" auto-play>
+						  	 <img src="images/header-bg.jpg">
+						  </div>
+						  
+						  <div class="carousel-cell" auto-play >
+						  	<img src="images/header-bg.jpg">
+						  </div>
+						   <div class="carousel-cell" auto-play>
+						  	<img src="images/header-bg.jpg">
+						  </div>
+
+					</div>
+
+					
+
+	  		</div>
+		</div>
+
+		<div class="container slide2">
+			
+	  		<div class="row">
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>Welcome to our portal</h2>
+						  
+		  			</div>
+					  A library is a pool of sources of information. This similar resources had made a well-defined community including readers, students etc to refer or to borrow the book more conveniently. The Library Management System Software for Library Management is used to find books and access journals easily. The library automation system automates the typical procedures of libraries and reduces the workload for library staffs. It makes the consistency of the record and the standard quality. When people value information more and more, the information industry got developed and the technology changed the expectations of library patrons. It gives both an opportunity and challenge to the libraries. The integrated library system is used to manage the more-complex activities and it enables the librarian to manage library resources in a more effective way to save time and effort.
+	  				
+	  				<a href="#"><p class="slide2"><button class="btn btn-success">Sign Up</button></p></a>
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+		  			<div class="page-header col-lg-offset-1">
+		  				<h2>24/7 Operational Support</h2>
+		  			</div>
+	  				A digital library, also called an online library, an internet library, a digital repository, or a digital collection is an online database of digital objects that can include text, still images, audio, video, digital documents, or other digital media formats or a library accessible through the internet. Objects can consist of digitized content like print or photographs, as well as originally produced digital content like word processor files or social media posts. In addition to storing content, digital libraries provide means for organizing, searching, and retrieving the content contained in the collection.
+	  			</div>
+	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
+	  				<div class="page-header col-lg-offset-1">
+	  				<h2>Why Us?</h2>
+	  			</div>
+				  A library management system is the most proficient and easy to use system for managing all the processes involved in a Library in the most effective ways.
+                  This system will reduce all the manual work and the whole process can be managed just through single clicks and edits.
+                  There will be no headache and doubtfulness of storing the data securely and searching the records of any individual afterward.
+                  Any book seeker can rent a book just by signing in with their details, and return it with the date of returning.
+                  The staff can also facilitate themselves with some extra authorizations and privileges.
+                  Only, one person is required to take care of the whole system, without any chances of mistakes.
+	  			</div>
+	  		</div>
+		</div>
+
+		<div class="container-fluid slide3" style="background-color: #282828">
+			<div class="container">
+				<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<a href="#" class="thumbnail">
+						<img src="images/thumbnail.jpg">
+					</a>
+				</div>
+			</div>
+			</div>
+			
+		</div>
+		<div class="container_fluid">
+			<p class="page-header center-block">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				
+			</p>
+			<p class="page-header center-block">
+				<a href="CONTACTus.php"><button class="btn btn-primary">Contact Us</button></a>
+				<a href="aboutus.php"><button class="btn btn-info">About Us</button></a>
+
+			</p>
+		</div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
