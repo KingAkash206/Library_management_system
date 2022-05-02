b6b3f80d5b2e680b0d81cca9a19ccabf92524ef4
Akash Kumar Mohanty <104777123+KingAkash206@users.noreply.github.com>
2022-05-02 20:21:46 +0530

    Add files via upload

---
+++ CONTACTus.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>contact-us</title>
+    <style>
+        .design label{
+            width:100%;
+            padding:5px;
+            background-color:black;
+            color:white;
+            font-family:"poppins",sans-serif;
+            margin-left:30px;
+            margin-top:200px;
+            font-size:x-large;
+        }
+        .design{
+            font-family:"poppins",sans-serif;
+            font-size:small;
+            margin-left:500px;
+            margin-top:200px;
+        }
+    </style>
+</head>
+<body>
+    <div class="design">
+        email: <br><br>
+        <label for="email">basudevbiswal35@gmail.com</label>
+        <br><br>
+        phone.no.: <br><br>
+        <label for="phoneno">8480946078</label>
+        <br><br><br>
+        We are available from monday to saturday ;from 9:00am to 9:00pm
+    </div>
+</body>
+</html>
---
+++ aboutus.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style>
+        .about{
+            margin-left: 300px;
+            margin-right: 300px;
+            margin-top: 100px;
+            background-color: darkblue;
+            color: white;
+            opacity: 0.5;
+            border-radius: 25px;
+            transition: 0.3s;
+            padding: 100px;
+        }
+        .about:hover{
+            background-color: darkblue;
+
+        }
+    </style>
+</head>
+<body>
+    <div class="about">
+        we are the most abundant library managemengt team.you can contact us for more details .we provide a wide range of books from differnt authors and diferent publishers.we also provide foreign authos book and in a large amount.you can easily issue and take book with you provided that you should work according to our terms and conditions.
+    </div>
+</body>
+</html>
